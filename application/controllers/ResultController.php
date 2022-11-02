27251a2fb65493f796a0785dab6efaa16199c4fc
kenjiposeidon <i.am.kenji.gido@gmail.com>
2022-11-02 12:57:45 +0900

    :construction: ResultController.php

--- application/controllers/ResultController.php
+++ application/controllers/ResultController.php
@@ -2,8 +2,10 @@
 declare(strict_types=1);
 
 /**
- * @property Twig $twig
- * @property CI_Input $input
+ * @property Twig       $twig
+ * @property CI_Input   $input
+ * @property CI_Config  $config
+ * @property DrinkModel $drinkModel
  */
 class ResultController extends CI_Controller
 {
@@ -11,13 +13,31 @@ class ResultController extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->config('data');
+        $this->load->model([
+            'drinkModel'
+        ]);
     }
 
     public function index(): void
     {
+        $drink_customize_set = $this->config->item('drink_customize_set');
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $imageUrl = $_POST['imageUrl'];
+        $selectedPrice = $_POST['selectedPrice'];
+
         $assign = [
-            'aaa' => 'a'
+            'sets' => $drink_customize_set[$name],
+            'name' => $name,
+            'price' => $price,
+            'selectedPrice' => $selectedPrice,
+            'imageUrl' => $imageUrl,
         ];
+
+        echo '<pre>';
+        print_r($assign);
+        exit();
 
         $this->twig->render('/templates/result.html.twig', $assign);
     }
