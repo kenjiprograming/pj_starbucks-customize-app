055d4f91cb8b5bd4563626b97041d84bf2e60130
kenjiposeidon <i.am.kenji.gido@gmail.com>
2022-11-02 12:56:36 +0900

    :sparkles: SearchCotroller.php

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -2,19 +2,42 @@
 declare(strict_types=1);
 
 /**
- * @property $twig
+ * @property DrinkModel $drinkModel
+ * @property CI_Config  $config
  */
 class SearchController extends CI_Controller
 {
-
     public function __construct()
     {
         parent::__construct();
+        $this->load->config('data');
     }
 
-    public function index($drink_name): void
+    public function index($kind): void
     {
-        $this->twig->render('/templates/search.html.twig');
+        $drinks = $this->config->item('drinks');
+        $classifiedDrinks = $this->classifyInKinds($drinks);
+
+        $assign = [
+            'drinks' => $classifiedDrinks[$kind]
+        ];
+
+        $this->twig->render('/templates/search.html.twig', $assign);
+    }
+
+    private function classifyInKinds($drinks)
+    {
+        $kinds = $this->config->item('kinds');
+
+        $classifiedDrinkData = [];
+
+        foreach ($kinds as $kind) {
+            $classifiedDrinkData[$kind] = array_filter($drinks, static function ($drink) use ($kind) {
+                return $drink['kind'] === $kind;
+            });
+        }
+
+        return $classifiedDrinkData;
     }
 
 }
